c719af5247a907c03118a5ca6498970ca69f70f4
Rahul181007 <rahulsheeraj@gmail.com>
2026-06-10 21:46:53 +0530

    Configure CORS for production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL,
     credentials: true,
   });
   app.useGlobalPipes(
